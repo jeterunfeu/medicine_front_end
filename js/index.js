a386fa2f9ff1c6047dfed87f4b1c1b14e2acc31c
SES-2301\user <whgpfl45612312@gmail.com>
2023-04-28 08:53:37 +0900

    인덱스, css, js 로딩화면 추가함

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-var _showPage = function() {
-    var loader = $("div.loader");
-    var container = $("div.container");
-    loader.css("display","none");
-    container.css("display","block");
-};
-// $(window).load(function(){
-//     $('.loader').delay(2000).fadeOut();
-// })
+// laoding 
+$(window).load(function(){
+    $('.loading_container').delay(2000).fadeOut();
+})
